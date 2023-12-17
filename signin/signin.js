60b710c5d1161eb7b25b20c1fef1853ec51a2186
midcondria <hyukkind@naver.com>
2023-12-17 20:15:46 +0900

    feature : 로그인 페이지 비밀번호 관련 오류 메시지 추가
    - 비밀번호 입력값이 빈 값일 경우
      - "비밀번호를 입력해주세요." 메시지 출력

--- signin/signin.js
+++ signin/signin.js
@@ -1,18 +1,22 @@
 const signinBox = document.querySelector(".signin-box");
 const emptyEmail = document.querySelector(".email-empty");
 const wrongEmail = document.querySelector(".email-wrong");
+const emptyPassword = document.querySelector(".password-empty");
 
 function emailValidation(e) {
   const type = e.target.type;
   const value = e.target.value;
   let regex = new RegExp("[a-z0-9]+@[a-z]+.[a-z]{2,3}");
-  console.log();
 
   if (type == "email") {
     if (Number(value) == 0) {
       emptyEmail.classList.remove("hidden");
     } else if (!regex.test(value)) {
       wrongEmail.classList.remove("hidden");
+    }
+  } else if (type == "password") {
+    if (Number(value) == 0) {
+      emptyPassword.classList.remove("hidden");
     }
   }
 }
@@ -22,6 +26,8 @@ function reset(e) {
   if (type == "email") {
     emptyEmail.classList.add("hidden");
     wrongEmail.classList.add("hidden");
+  } else if (type == "password") {
+    emptyPassword.classList.add("hidden");
   }
 }
 

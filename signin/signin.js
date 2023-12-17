3ef0f7659889883e1ec84074170b94639aa6b3c5
midcondria <hyukkind@naver.com>
2023-12-17 21:36:54 +0900

    feature : 로그인 처리 기능 추가
    - 성공 시 /foreder 로
    - 실패 시 input 태그 아래에 오류메시지 출력

--- signin/signin.js
+++ signin/signin.js
@@ -2,8 +2,17 @@ const signinBox = document.querySelector(".signin-box");
 const emptyEmail = document.querySelector(".email-empty");
 const wrongEmail = document.querySelector(".email-wrong");
 const emptyPassword = document.querySelector(".password-empty");
+const signinBtn = document.querySelector(".signin-btn");
+const notValidEmail = document.querySelector(".not-valid-email");
+const notValidPassword = document.querySelector(".not-valid-password");
 
-function emailValidation(e) {
+const emailInput = document.querySelector(".sign-input-email");
+const passwordInput = document.querySelector(".sign-input-password");
+
+const TEST_EMAIL = "test@codeit.com";
+const TEST_PW = "codeit101";
+
+function emailCheck(e) {
   const type = e.target.type;
   const value = e.target.value;
   let regex = new RegExp("[a-z0-9]+@[a-z]+.[a-z]{2,3}");
@@ -23,6 +32,8 @@ function emailValidation(e) {
 
 function reset(e) {
   const type = e.target.type;
+  notValidEmail.classList.add("hidden");
+  notValidPassword.classList.add("hidden");
   if (type == "email") {
     emptyEmail.classList.add("hidden");
     wrongEmail.classList.add("hidden");
@@ -31,5 +42,26 @@ function reset(e) {
   }
 }
 
+function validation() {
+  const email = emailInput.value;
+  const password = passwordInput.value;
+  if (email == TEST_EMAIL && password == TEST_PW) {
+    window.location.href = "/forder.html";
+  } else {
+    notValidEmail.classList.remove("hidden");
+    notValidPassword.classList.remove("hidden");
+    console.log("돌아가");
+  }
+}
+
+function validationByEnter(e) {
+  if (e.key === "Enter") {
+    console.log("동작은함");
+    validation();
+  }
+}
+
 signinBox.addEventListener("focusin", reset);
-signinBox.addEventListener("focusout", emailValidation);
+signinBox.addEventListener("focusout", emailCheck);
+signinBox.addEventListener("keydown", validationByEnter);
+signinBtn.addEventListener("click", validation);

32ce5566aedc051f31f87d9109439b79bc65df3f
midcondria <hyukkind@naver.com>
2023-12-31 00:24:07 +0900

    feature : Card 컴포넌트 v1
    - 이미지, 설명, 등록일자 까지는 불러옴
    - CSS 는 입혀야함
    - 날짜 계산 로직도 손봐야함

--- src/components/LinkItem.js
+++ src/components/LinkItem.js
@@ -1,8 +1,52 @@
 import Card from "./Card";
 import styles from "./LinkItem.module.css";
 
+const DEFAULT_IMAGE =
+  "https://i.namu.wiki/i/1KSjLQJ6kGnGlCaxPjhO1EzAiwCaIOSCkZsK6qd3T_bJARHiqd1XsHtZT3bKvEP_zjn6uZklJ6MsNT67pYSr4V3sQ_HRXSvTVvOu7T1ZBltd-nk7SU9RgFtIx0Yr9yOcZ4EgxCnYdoBs3oqnLALpCA.webp";
+
+function calculateTimeDifference(input) {
+  const targetDate = new Date(input);
+  const currentDate = new Date();
+  const timeDifference = currentDate - targetDate;
+
+  const seconds = Math.floor(timeDifference / 1000);
+  const minutes = Math.floor(seconds / 60);
+  const hours = Math.floor(minutes / 60);
+  const days = Math.floor(hours / 24);
+  const months = Math.floor(days / 31);
+
+  //   console.log(
+  //     "sec: " + seconds,
+  //     "mins: " + minutes,
+  //     "hours: " + hours,
+  //     "days: " + days,
+  //     "months: " + months
+  //   );
+  return `${days}일 전`;
+}
+
+function formatDate(input) {
+  const date = new Date(input);
+  return `${date.getFullYear()}. ${date.getMonth() + 1}. ${date.getDate()}`;
+}
+
 function LinkItem({ linkInfo }) {
-  return <Card>info : {linkInfo.id}</Card>;
+  return (
+    <Card>
+      <div className={styles.thumb}>
+        <img
+          width="320px"
+          height="220px"
+          src={linkInfo.imageSource || DEFAULT_IMAGE}
+        />
+      </div>
+      <div className={styles.content}>
+        <p>{calculateTimeDifference(linkInfo.createdAt)}</p>
+        <p>{linkInfo.description}</p>
+        <p>{formatDate(linkInfo.createdAt)}</p>
+      </div>
+    </Card>
+  );
 }
 
 export default LinkItem;

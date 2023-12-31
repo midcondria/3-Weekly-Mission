59444b82fec4c7646569758de9beda6347149e69
midcondria <hyukkind@naver.com>
2023-12-31 22:51:55 +0900

    feature : 검색창 틀만 구현
    - 기능은 이후 추가

--- src/pages/FolderListPage.js
+++ src/pages/FolderListPage.js
@@ -1,23 +1,31 @@
 import searchBarStyles from "../components/SearchBar.module.css";
 import ListPage from "../components/ListPage";
 import LinkItem from "../components/LinkItem";
+import searchIcon from "../assets/Search.png";
 
 function FolderListPage({ folders }) {
   console.log(folders?.folder);
   const favoriteFolder = folders?.folder;
   return (
-    <ListPage userInfo="">
-      <div style={{ textAlign: "center" }}>FolderListPage</div>
-      <form className={searchBarStyles.form}>
-        <input name="search" placeholder="링크를 검색해 보세요." />
-      </form>
-      <div>
-        {favoriteFolder &&
-          favoriteFolder.links.map((linkInfo) => (
-            <LinkItem linkInfo={linkInfo}></LinkItem>
-          ))}
-      </div>
-    </ListPage>
+    <div style={{ textAlign: "center" }}>
+      FolderListPage
+      <ListPage userInfo="">
+        <form className={searchBarStyles.form}>
+          <input name="search" placeholder="링크를 검색해 보세요."></input>
+          <img
+            className={searchBarStyles.searchIcon}
+            src={searchIcon}
+            alt="검색창 아이콘"
+          />
+        </form>
+        <div>
+          {favoriteFolder &&
+            favoriteFolder.links.map((linkInfo) => (
+              <LinkItem linkInfo={linkInfo}></LinkItem>
+            ))}
+        </div>
+      </ListPage>
+    </div>
   );
 }
 

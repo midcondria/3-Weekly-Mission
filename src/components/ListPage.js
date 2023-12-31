8c25832048d7c3f200b2be955440c15a458c7af4
midcondria <hyukkind@naver.com>
2023-12-31 22:51:55 +0900

    feature : 검색창 틀만 구현
    - 기능은 이후 추가

--- src/components/ListPage.js
+++ src/components/ListPage.js
@@ -1,12 +1,13 @@
+import Container from "./Container";
 import styles from "./ListPage.module.css";
 
 function ListPage({ userInfo, children }) {
   console.log(userInfo);
   return (
-    <div style={{ textAlign: "center" }}>
-      ListPage
-      <div>{children}</div>
-    </div>
+    <>
+      <div style={{ textAlign: "center" }}>ListPage</div>
+      <Container className={styles.container}>{children}</Container>
+    </>
   );
 }
 

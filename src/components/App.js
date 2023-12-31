edcfab915f15facc8c52ae5f23193cc23d44c9af
midcondria <hyukkind@naver.com>
2023-12-31 20:44:10 +0900

    feature : Nav 바 구현
    - 이미지를 사용하기 위해 정적 이미지 저장 경로를 기존 /images -> /src/assets 폴더로 이동
    - Nav 바 구현
    - 유저 정보를 받기 위한 API 요청 함수 getUserProfile() 추가
    - 유저 정보 유무에 따라 로그인 버튼 / 유저 정보 표시

--- src/components/App.js
+++ src/components/App.js
@@ -4,17 +4,21 @@ import FolderListPage from "../pages/FolderListPage";
 import styles from "./App.module.css";
 import Footer from "./Footer";
 import Nav from "./Nav";
-import { getFolders } from "../api/api";
+import { getFolders, getUserProfile } from "../api/api";
 
 function App() {
   const [folders, setFolders] = useState([]);
+  const [userProfile, setUserProfile] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const result = await getFolders();
-        if (!result) return;
-        setFolders(result);
+        const folders = await getFolders();
+        const userProfile = await getUserProfile();
+
+        if (!folders) return;
+        setFolders(folders);
+        setUserProfile(userProfile);
       } catch (error) {
         console.log(error);
       }
@@ -24,7 +28,7 @@ function App() {
 
   return (
     <div>
-      <Nav />
+      <Nav userProfile={userProfile} />
       <div className={styles.a}>hi</div>
       <FolderListPage folders={folders} />
       <div className={styles.a}>bye</div>

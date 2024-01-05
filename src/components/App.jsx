7822f390790ab2d7911120ec06b766427d40b739
midcondria <hyukkind@naver.com>
2024-01-05 15:53:03 +0900

    refactor : Header / Body / Footer 구조로 분리 및 UI 로직 분리
    - 기존 Nav / Body / Footer 구조에서 Header / Body / Footer 로 변경
    - App.jsx 컴포넌트에서 처리하던 헤더에서 쓰던 유저정보 API 호출을 Nav
      컴포넌트에서 처리하도록 변경

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,22 +3,19 @@ import { useEffect, useState } from "react";
 import FolderListPage from "../pages/FolderListPage";
 import styles from "./App.module.css";
 import Footer from "./Footer";
-import Nav from "./Nav";
-import { getFolders, getUserProfile } from "../api/api";
+import { getFolders } from "../api/api";
+import Header from "./Header";
 
 function App() {
   const [folders, setFolders] = useState([]);
-  const [userProfile, setUserProfile] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const folders = await getFolders();
-        const userProfile = await getUserProfile();
 
         if (!folders) return;
         setFolders(folders);
-        setUserProfile(userProfile);
       } catch (error) {
         console.log(error);
       }
@@ -28,7 +25,7 @@ function App() {
 
   return (
     <>
-      <Nav userProfile={userProfile} />
+      <Header />
       <div className={styles.body}>
         <FolderListPage folders={folders} />
       </div>
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,7 +2,25 @@ import Container from "./Container";
 import styles from "./Nav.module.css";
 import logoImg from "../assets/logo.svg";
 import UserMenu from "./UserMenu";
-function Nav({ userProfile }) {
+import { useEffect, useState } from "react";
+import { getUserProfile } from "../api/api";
+
+function Nav() {
+  const [userProfile, setUserProfile] = useState(null);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const userProfile = await getUserProfile();
+
+        if (!userProfile) return;
+        setUserProfile(userProfile);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <div className={styles.nav}>
       <Container className={styles.container}>

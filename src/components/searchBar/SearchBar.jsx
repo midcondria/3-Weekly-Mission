74a9cb5e9da778f8edf72a8d6e8f3f4adbb28d4a
midcondria <hyukkind@naver.com>
2024-02-19 00:46:10 +0900

    feat: X 버튼 클릭 시 검색어 초기화 기능 추가

--- src/components/searchBar/SearchBar.jsx
+++ src/components/searchBar/SearchBar.jsx
@@ -1,7 +1,7 @@
 import styles from "./SearchBar.module.css";
 import searchIcon from "../../assets/Search.png";
 import { useState } from "react";
-import closeSvg from "../../assets/close.svg";
+import { ReactComponent as CloseSvg } from "../../assets/close.svg";
 
 export default function SearchBar({ onFilter }) {
   const [value, setValue] = useState("");
@@ -15,15 +15,29 @@ export default function SearchBar({ onFilter }) {
     setValue(e.target.value);
   };
 
+  const handleResetClick = (e) => {
+    e.preventDefault();
+    setValue("");
+  };
+
   return (
     <form className={styles.form} onSubmit={handleSubmit}>
+      <img className={styles.searchIcon} src={searchIcon} alt="검색창 아이콘" />
       <input
         name="search"
         value={value}
         placeholder="링크를 검색해 보세요."
         onChange={handleChange}
       />
-      <img className={styles.searchIcon} src={searchIcon} alt="검색창 아이콘" />
+      {value.length > 0 && <ResetButton onClick={handleResetClick} />}
     </form>
+  );
+}
+
+function ResetButton({ onClick }) {
+  return (
+    <button type="button" className={styles.resetButton} onClick={onClick}>
+      <CloseSvg />
+    </button>
   );
 }
